0fec1392741bb7851c99582ac5aae6970eda0904
Bruno <brunodorea@outlook.com.br>
2025-02-11 09:41:42 -0300

    :test_tube: test: att script de teste

--- tests/script.js
+++ tests/script.js
@@ -2,16 +2,17 @@ import http from 'k6/http'
 import { check, sleep } from 'k6'
 
 export const options = {
-    vus: 1,
-    duration: '10s',
+    vus: 10,
+    duration: '30s',
     thresholds: {
         'http_req_duration': ['p(95)<2000'], /// 95% das requisições devem responder em menos de 2s
-        'http_req_failed': ['rate<0.01'] // até 1% das requisições podem falhar
+        'http_req_failed': ['rate<0.01'], // até 1% das requisições podem falhar
+        checks: ['rate > 0.99']
     }
 }
 
 export default function (data) {
-    const url = ""
+    const url = "https://test-api.k6.io/public/crocodiles/"
     const params = { headers: { "Content-Type": "application/json", } }
     const response = http.get(url, params)
 
